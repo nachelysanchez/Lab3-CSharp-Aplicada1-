8f2612432d7e2d036094275d160fa618ae983ffc
nachelysanchez <nashely-5@hotmail.com>
2021-09-19 00:18:56 -0400

    Tarea 3

---
+++ Tarea3/UsuariosControlWPF/DAL/Contexto.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UsuariosControlWPF.Entidades;
+
+namespace UsuariosControlWPF.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Roles> Roles { get; set; }
+        public DbSet<Usuarios> Usuarios { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = DATA\UserControl.db");
+        }
+        /*protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Roles>().HasData(new Roles
+            {
+                RolId = 1,
+                FechaCreacion = DateTime.Now,
+                Descripcion = "Administrador"
+            });
+        }*/
+    }
+}
